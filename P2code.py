999795b6b60ea7372b3bfce8aee442af753c554d
lsun1996 <123316243+lsun1996@users.noreply.github.com>
2023-10-06 18:27:10 -0400

    Update P2code.py

--- P2code.py
+++ P2code.py
@@ -64,7 +64,7 @@ def random_weighted_graph(num_vertices, num_edges):
 
     return graph
 
-# use union find to check if two vertices are connected
+# use union find to check if two vertices are connected and connect vertices added to the mst
 class UnionFind(object):
     def __init__(self, size):
         self.root = [i for i in range(size)]
@@ -92,8 +92,8 @@ class UnionFind(object):
     def connected(self, x, y):
         return self.find(x) == self.find(y)
 
-graphs = []
-
+graphs = [] # this list saves all test cases
+# all test cases have 500 vertices, and the edge number varies from 600 - 9600
 for i in range(600, 9900, 500):
     graph = random_weighted_graph(500, i)
     graphs.append(graph)
@@ -103,14 +103,14 @@ test_case = 0
 for graph in graphs:
     start_time = time.time()
     test_case += 1
-    edges = sorted(graph.get_edges(), key=lambda x: x[-1])
+    edges = sorted(graph.get_edges(), key=lambda x: x[-1]) # sort all edges by weight
     vertices = graph.get_vertices()
     mst = []
-    uf = UnionFind(len(vertices))
+    uf = UnionFind(len(vertices)) # initialize a unionfind data structure of size len(vertices)
     for edge in edges:
-        if uf.count == 1:
+        if uf.count == 1: # uf.count == 1 means all vertices are connected, mst found
             break
-        if not uf.connected(edge[0], edge[1]):
+        if not uf.connected(edge[0], edge[1]): # if the two vertices are connected in union find, adding this edge will create a cycle
             mst.append(edge)
             uf.union(edge[0], edge[1])
     end_time = time.time()
